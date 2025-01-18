0aaa7ec787febc3ae0cc3022acec799dc982aa08
Rohit Dey <102870148+Rohitdey09@users.noreply.github.com>
2025-01-18 23:43:24 +0530

    Add files via upload

---
+++ Speech_to_Txt.py
@@ -0,0 +1,8 @@
+##Speech to Text
+
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("SPEAK.....")
+    audio = r.listen(source)
+print(r.recognize_google(audio))    
